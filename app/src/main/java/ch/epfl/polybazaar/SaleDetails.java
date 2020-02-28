7006e40789f7391b81769ed590ca9e8aff6d2e4a
walterieux <david.desboeufs@hotmail.fr>
2020-02-28 14:00:21 +0100

    return button

--- app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
+++ app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
@@ -2,7 +2,10 @@ package ch.epfl.polybazaar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SaleDetails extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SaleDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sale_details);
+
+        Button ret = (Button) findViewById(R.id.ret_but);
+        ret.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Listing.class);
+                startActivityForResult(myIntent, 0);
+            }
     }
 }
