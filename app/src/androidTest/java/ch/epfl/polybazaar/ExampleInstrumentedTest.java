576b47b229d229fcbc4fbe71c1b7888e4929d2aa
walterieux <david.desboeufs@hotmail.fr>
2020-02-28 15:40:50 +0100

    set the two buttons action

--- app/src/androidTest/java/ch/epfl/polybazaar/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ch/epfl/polybazaar/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ch.epfl.sdp", appContext.getPackageName());
+        assertEquals("ch.epfl.polybazaar", appContext.getPackageName());
     }
 }
--- app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
+++ app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
@@ -2,10 +2,13 @@ package ch.epfl.polybazaar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SaleDetails extends AppCompatActivity {
 
@@ -14,11 +17,46 @@ public class SaleDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sale_details);
 
+        ImageView image = (ImageView)findViewById(R.id.imageView2);
+        image.setImageResource(R.drawable.algebre_lin);
+
+
+        TextView title_txt = (TextView)findViewById(R.id.title);
+        title_txt.setText("Algèbre Linéaire by David C. Lay");
+
+        TextView description_txt = (TextView)findViewById(R.id.description);
+        description_txt.setText("Never used");
+
+        TextView price_txt = (TextView)findViewById(R.id.price);
+        price_txt.setText("12.-");
+
         Button ret = (Button) findViewById(R.id.ret_but);
+        Button get_seller = (Button) findViewById(R.id.contact_sel);
+
         ret.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Listing.class);
-                startActivityForResult(myIntent, 0);
+                //Intent myIntent = new Intent(view.getContext(), SalesOverview.class);
+                //startActivityForResult(myIntent, 0);
+                
+
             }
+        });
+
+        get_seller.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                
+
+            }
+        });
+
+
+
+        /*
+        ret.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //Intent myIntent = new Intent(view.getContext(), ???.class); //create a class to display info? Or put a TextView at the place of the button?
+                //startActivityForResult(myIntent, 0);
+            }
+        });*/
     }
 }
