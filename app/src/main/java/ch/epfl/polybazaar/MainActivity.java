d984985e415ae1d7b2ddba4204c305442c3f5956
walterieux <david.desboeufs@hotmail.fr>
2020-02-28 22:37:33 +0100

    remove modifications on MainActivity.java

--- app/src/main/java/ch/epfl/polybazaar/MainActivity.java
+++ app/src/main/java/ch/epfl/polybazaar/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent =  new Intent(MainActivity.this, SaleDetails.class);
-        startActivity(intent);
     }
 }
