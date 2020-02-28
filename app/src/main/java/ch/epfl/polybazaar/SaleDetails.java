a14712a6e107c8ca1f32645c436d687a6dc4ba84
walterieux <david.desboeufs@hotmail.fr>
2020-02-28 13:43:26 +0100

    design of the layout

--- app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
+++ app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
@@ -1,14 +1,14 @@
-package ch.epfl.sdp;
+package ch.epfl.polybazaar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SaleDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sale_details);
     }
 }
