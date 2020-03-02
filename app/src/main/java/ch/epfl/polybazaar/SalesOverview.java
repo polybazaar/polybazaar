8684d4edd8f97462bc5ee240d815a9df7b6fbf09
armen-h <armen.homberger@epfl.ch>
2020-03-02 22:09:44 +0100

    implement a 3 objects display

---
+++ app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
@@ -0,0 +1,14 @@
+package ch.epfl.polybazaar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SalesOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_overview);
+    }
+}
