cf37520798ac719931ddc02076c60b743fcbb14e
Benoit <benoit.maillard@epfl.ch>
2020-03-02 18:53:49 +0100

    Add basic sign-in activity

---
+++ app/src/main/java/ch/epfl/polybazaar/login/SignInActivity.java
@@ -0,0 +1,16 @@
+package ch.epfl.polybazaar.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.epfl.polybazaar.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
