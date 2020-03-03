275cb85302e6794855f3ee77dafc02726b0e5de6
Benoit <benoit.maillard@epfl.ch>
2020-03-03 09:49:43 +0100

    Add basic sign-in functionnality

--- app/src/main/java/ch/epfl/polybazaar/login/SignInActivity.java
+++ app/src/main/java/ch/epfl/polybazaar/login/SignInActivity.java
@@ -1,16 +1,64 @@
 package ch.epfl.polybazaar.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import ch.epfl.polybazaar.R;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private static final String TAG = "SignInActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), SignInSuccessActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void submit(View view) {
+        EditText emailView = findViewById(R.id.emailInput);
+        EditText passwordView = findViewById(R.id.passwordInput);
+
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), SignInSuccessActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Log.e(TAG, "Unable to sign in!");
+                        }
+                    }
+                });
     }
 }
