83089f709c3cb799755c7cc9b137981532689acc
armen-h <armen.homberger@epfl.ch>
2020-03-03 13:38:28 +0100

    implement graphical listing view (SalesOverview Class) that is linked to the listing details class (SaleDetails Class).

---
+++ app/src/androidTest/java/ch/epfl/polybazaar/SalesOverviewTest.java
@@ -0,0 +1,54 @@
+package ch.epfl.polybazaar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SalesOverviewTest {
+
+    @Rule
+    public final ActivityTestRule<SalesOverview> activityRule =
+            new ActivityTestRule<>(
+                    SalesOverview.class,
+                    true,
+                    false);
+
+    @Test
+    public void testOnCreate() {
+        Intent intent = new Intent();
+        Bundle b = new Bundle();
+        b.putString("title1", "Algebre Linéaire by David C. Lay" );
+        b.putString("price1", "CHF 13");
+        intent.putExtras(b);
+
+        activityRule.launchActivity(intent);
+        TextView text_title = (TextView)activityRule.getActivity().findViewById(R.id.title1);
+        assertEquals("Algebre Linéaire by David C. Lay", text_title.getText().toString());
+
+        TextView text_price = (TextView)activityRule.getActivity().findViewById(R.id.price1);
+        assertEquals("CHF 13", text_price.getText().toString());
+    }
+
+    @Test
+    public void testOnCreateThrowAnException() {
+        Intent intent = new Intent();
+        boolean exceptionThrown = false;
+
+        try {
+            Bundle b = new Bundle();
+            activityRule.getActivity().onCreate(b);
+        }catch (NullPointerException e) {
+            exceptionThrown = true;
+        }
+
+        assertEquals(true, exceptionThrown);
+    }
+
+}
--- app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
+++ app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
@@ -1,6 +1,8 @@
 package ch.epfl.polybazaar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,17 @@ public class SalesOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_overview);
+
+        // TODO: fetch listing attributes from database and dynamically build the overview
     }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(SalesOverview.this, SaleDetails.class);
+        intent.putExtra("title", "Algebre Linéaire by David C. Lay" );
+        intent.putExtra("description", "Never used");
+        intent.putExtra("price", "18 CHF");
+        startActivity(intent);
+    }
+
+
 }
