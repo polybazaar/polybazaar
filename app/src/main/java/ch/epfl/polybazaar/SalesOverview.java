9ac6f0778e8074301484e995a3cffa57ac6baa42
armen-h <armen.homberger@epfl.ch>
2020-03-04 17:36:13 +0100

    implement dynamic display of title of clicked item

--- app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
+++ app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
@@ -3,6 +3,7 @@ package ch.epfl.polybazaar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,8 +18,9 @@ public class SalesOverview extends AppCompatActivity {
     }
 
     public void onClick(View v) {
+        TextView itemName = findViewById(v.getId());
         Intent intent = new Intent(SalesOverview.this, SaleDetails.class);
-        intent.putExtra("title", "Algebre Linéaire by David C. Lay" );
+        intent.putExtra("title", itemName.getText().toString());
         intent.putExtra("description", "Never used");
         intent.putExtra("price", "18 CHF");
         startActivity(intent);
