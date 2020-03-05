2169d219402be225552fbfa2b524a62748f848f9
Benoit <benoit.maillard@epfl.ch>
2020-03-05 07:59:29 +0100

    Add sign up activity

---
+++ app/src/main/java/ch/epfl/polybazaar/login/SignUpActivity.java
@@ -0,0 +1,105 @@
+package ch.epfl.polybazaar.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import ch.epfl.polybazaar.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), SignInSuccessActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void submit(View view) {
+        EditText emailView = findViewById(R.id.emailInput);
+        EditText passwordView = findViewById(R.id.passwordInput);
+        EditText confirmPasswordView = findViewById(R.id.confirmPasswordInput);
+
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+        String confirmPassword = confirmPasswordView.getText().toString();
+
+        if (!checkEmail(email)) {
+            showErrorDialog(
+                    "Invalid e-mail",
+                    "Please enter a valid EPFL e-mail address"
+            );
+        } else if (!checkPassword(password)) {
+            showErrorDialog(
+                    "Invalid password",
+                    "Please make sure that both passwords match"
+            );
+        } else {
+            createUser(email, password);
+        }
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent intent = new Intent(getApplicationContext(), SignInSuccessActivity.class);
+                            startActivity(intent);
+                        } else {
+                            showErrorDialog("Sign up failed", "Please try again");
+                        }
+                    }
+                });
+    }
+
+    private boolean checkPassword(String password) {
+        return true;
+    }
+
+    private boolean checkEmail(String email) {
+        return true;
+    }
+
+    private void showErrorDialog(String title, String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+        builder.setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.create().show();
+    }
+}
