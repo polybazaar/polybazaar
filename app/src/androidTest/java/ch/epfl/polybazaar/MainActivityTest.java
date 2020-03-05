1e71e9413a306c9ee14d32f59be0277d61f87ecf
walterieux <david.desboeufs@hotmail.fr>
2020-03-05 16:35:21 +0100

    reverse change

--- app/src/androidTest/java/ch/epfl/polybazaar/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polybazaar/MainActivityTest.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -21,7 +19,6 @@ import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.r
 import static org.hamcrest.core.IsNot.not;
 
 public class MainActivityTest{
-    private Intent intent;
 
     @Rule
     public final ActivityTestRule<MainActivity> activityRule =
@@ -30,21 +27,13 @@ public class MainActivityTest{
                     true,
                     false);
 
-    public void init() {
-        Intents.init();
-        intent = new Intent();
-        activityRule.launchActivity(intent);
-    }
-
-
-    public void finalize() {
-        Intents.release();;
-    }
 
     @Test
     public void testStartSaleOverview() throws Throwable {
-        init();
-        //Use runOnUiThread because of the small screen on travis tests
+        Intents.init();
+        Intent intent = new Intent();
+
+        activityRule.launchActivity(intent);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -53,12 +42,15 @@ public class MainActivityTest{
             }
         });
         intended(hasComponent(SalesOverview.class.getName()));
-        finalize();
+        Intents.release();
     }
 
     @Test
     public void testStartFillListingActivity() throws Throwable {
-        init();
+        Intents.init();
+        Intent intent = new Intent();
+
+        activityRule.launchActivity(intent);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -67,14 +59,16 @@ public class MainActivityTest{
             }
         });
         intended(hasComponent(FillListingActivity.class.getName()));
-        finalize();
+        Intents.release();
     }
 
     @Test
     public void testStartSignIn() throws Throwable {
-        init();
         //TODO uncomment the code below
-        /*runOnUiThread(new Runnable() {
+        /*Intents.init();
+        Intent intent = new Intent();
+        activityRule.launchActivity(intent);
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 activityRule.getActivity().findViewById(R.id.add_listing)
@@ -82,9 +76,13 @@ public class MainActivityTest{
             }
         });
         // TODO complete
-        intended(hasComponent(<yourClass>.class.getName()));*/
+        intended(hasComponent(<yourClass>.class.getName()));
+        Intents.release();*/
 
         //TODO remove code below
+        Intent intent = new Intent();
+
+        activityRule.launchActivity(intent);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -96,7 +94,5 @@ public class MainActivityTest{
         onView(withText("This functionality is not implemented yet"))
                 .inRoot(withDecorView(not(activityRule.getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
-        // Don't remove this line
-        finalize();
     }
 }
