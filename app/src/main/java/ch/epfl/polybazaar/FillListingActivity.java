62581f779da2c13d60bc52e5ad42ace10c7387ce
Maxime Jan <maxime.jan@epfl.ch>
2020-03-08 19:28:52 +0100

    Add the option to take a picture with the camera

--- app/src/main/java/ch/epfl/polybazaar/FillListingActivity.java
+++ app/src/main/java/ch/epfl/polybazaar/FillListingActivity.java
@@ -2,11 +2,16 @@ package ch.epfl.polybazaar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +22,21 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import ch.epfl.polybazaar.listing.Listing;
 
 public class FillListingActivity extends AppCompatActivity {
 
     public static final int RESULT_LOAD_IMAGE = 1;
+    public static final int RESULT_TAKE_PICTURE = 2;
     public static final String INCORRECT_FIELDS_TEXT = "One or more required fields are incorrect or uncompleted";
 
     private Button uploadImage;
+    private Button camera;
     private Button submitListing;
     private ImageView pictureView;
     private Switch freeSwitch;
@@ -32,12 +44,14 @@ public class FillListingActivity extends AppCompatActivity {
     private EditText descriptionSelector;
     private EditText priceSelector;
     private String oldPrice;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_listing);
 
+        camera = findViewById(R.id.camera);
         pictureView = findViewById(R.id.picturePreview);
         freeSwitch = findViewById(R.id.freeSwitch);
         uploadImage = findViewById(R.id.uploadImage);
@@ -54,17 +68,28 @@ public class FillListingActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         //Tags are set only for testing purposes using Espresso
-        if(requestCode != RESULT_LOAD_IMAGE || resultCode != RESULT_OK || data==null){
+        if(resultCode != RESULT_OK){
             pictureView.setTag(-1);
         }
-        else{
+        else if (requestCode == RESULT_LOAD_IMAGE){
             Uri selectedImage = data.getData();
             pictureView.setImageURI(selectedImage);
             pictureView.setTag(selectedImage.hashCode());
         }
+        else if (requestCode == RESULT_TAKE_PICTURE){
+           pictureView.setImageURI(Uri.fromFile(new File(currentPhotoPath)));
+        }
     }
 
+
     private void addListeners(){
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
         freeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -85,6 +110,24 @@ public class FillListingActivity extends AppCompatActivity {
                 submit();
             }
         });
+    }
+
+    //Function taken from https://developer.android.com/training/camera/photobasics
+    private  void takePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"ch.epfl.polybazaar.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, RESULT_TAKE_PICTURE);
+            }
+        }
     }
 
     private void uploadImage(){
@@ -130,6 +173,23 @@ public class FillListingActivity extends AppCompatActivity {
         }
         Listing newListing = new Listing(titleSelector.getText().toString(), descriptionSelector.getText().toString(), priceSelector.getText().toString(), "test.user@epfl.ch");
 
+    }
+
+    //Function taken from https://developer.android.com/training/camera/photobasics
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 
