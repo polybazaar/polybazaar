eb0e71c1f1c0e4b18fba9127a191ba45b598ddf1
walterieux <david.desboeufs@hotmail.fr>
2020-03-09 14:15:48 +0100

    fetch data from dtb

--- app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
+++ app/src/main/java/ch/epfl/polybazaar/SaleDetails.java
@@ -10,45 +10,56 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ch.epfl.polybazaar.database.callback.ListingCallback;
+import ch.epfl.polybazaar.listing.Listing;
+import ch.epfl.polybazaar.listing.ListingDatabase;
+
 public class SaleDetails extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) throws NullPointerException{
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sale_details);
+
+        Listing listing;
 
         Bundle bundle = getIntent().getExtras();
         if(bundle == null)
             bundle = new Bundle();
 
         //get the uId of the object
-        int uId = bundle.getInt("uId", -1);
-        Listing listing = getObjectData(uId);
-
-        //set image
-        ImageView image = findViewById(R.id.imageView2);
-        int pic = bundle.getInt("image", -1);
-        if(pic != -1) {
-            image.setImageResource(pic);
+        String listingID = bundle.getString("listingID", "-1");
+        if(listingID.equals("-1")) {
+            throw new IllegalArgumentException();
         }
 
-        //Set the title
-        TextView title_txt = (TextView)findViewById(R.id.title);
-        title_txt.setText(listing.getTitle());
+        ListingDatabase ldb = new ListingDatabase("listing");
+        ListingCallback callbackListing = new ListingCallback() {
+            @Override
+            public void onCallback(Listing result) {
+                //set image
+                //ImageView image = findViewById(R.id.imageView2);
+                //image.setImageResource(result.getImage());
 
-        //Set the description
-        TextView description_txt = (TextView)findViewById(R.id.description);
-        description_txt.setText(listing.getDescription());
+                //Set the title
+                TextView title_txt = (TextView)findViewById(R.id.title);
+                title_txt.setText(result.getTitle());
 
-        //Set the price
-        TextView price_txt = (TextView)findViewById(R.id.price);
-        price_txt.setTextSize(20);
-        price_txt.setText(listing.getPrice());
+                //Set the description
+                TextView description_txt = (TextView)findViewById(R.id.description);
+                description_txt.setText(result.getDescription());
 
+                //Set the price
+                TextView price_txt = (TextView)findViewById(R.id.price);
+                price_txt.setTextSize(20);
+                price_txt.setText(result.getPrice());
+            }
+        };
+        ldb.fetchListing(listingID, callbackListing);
 
-        Button get_seller = findViewById(R.id.contact_sel);
 
         //Get seller action
+        Button get_seller = findViewById(R.id.contact_sel);
         get_seller.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 //TODO get the seller information through the listing
@@ -58,11 +69,5 @@ public class SaleDetails extends AppCompatActivity {
             }
         });
 
-    }
-
-    private Listing getObjectData(int uId) {
-        //get the information from the database
-        Listing listing = new Listing("Title", "Description", "Price");
-        return listing;
     }
 }
