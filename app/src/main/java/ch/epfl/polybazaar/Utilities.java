86d53fa708ae6c7af20fac8de3d10b380fd85a5f
walterieux <david.desboeufs@hotmail.fr>
2020-03-15 12:40:20 +0100

    add new utilities

--- app/src/main/java/ch/epfl/polybazaar/Utilities.java
+++ app/src/main/java/ch/epfl/polybazaar/Utilities.java
@@ -1,5 +1,15 @@
 package ch.epfl.polybazaar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import ch.epfl.polybazaar.user.User;
 
 public abstract class Utilities {
@@ -20,5 +30,82 @@ public abstract class Utilities {
         }
         return false;
     }
+
+    /**
+     * Convert a File to a String
+     * @param imageFile
+     * @return a String
+     */
+    public static String convertFileToString(File imageFile) {
+        String tempStringImg = "";
+        //inspired from https://stackoverflow.com/questions/13562429/how-many-ways-to-convert-bitmap-to-string-and-vice-versa/18052269
+        if(imageFile != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] b = baos.toByteArray();
+            tempStringImg = Base64.encodeToString(b, Base64.DEFAULT);
+        }
+        return tempStringImg;
+    }
+
+    /**
+     * Convert a String image to a Bitmap
+     * @param stringImage
+     * @return a Bitmap
+     */
+    public  static Bitmap convertStringToBitmap (String stringImage) {
+        if(stringImage == null || stringImage.equals("")) {
+            return null;
+        }
+        try {
+            byte [] encodeByte = Base64.decode(stringImage, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+    /**
+     * Convert Bitmap to String
+     * @param bitmap
+     * @return a String
+     * taken from Stackoverflow
+     */
+    public static String convertBitmapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp=Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+
+
+    public static Bitmap convertDrawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+
 
 }
