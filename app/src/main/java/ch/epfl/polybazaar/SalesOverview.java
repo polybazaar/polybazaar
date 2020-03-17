98f34289db336d22a2fa2d1766a3477251c8f708
Mathieu Caboche <mathieucaboche@gmail.com>
2020-03-17 12:57:25 +0100

    done except still cant delete users

--- app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
+++ app/src/main/java/ch/epfl/polybazaar/SalesOverview.java
@@ -181,6 +181,6 @@ public class SalesOverview extends AppCompatActivity {
                 }
             }
         };
-        //fetchLiteListingList(callbackLiteListingList);
+        fetchLiteListingList(callbackLiteListingList);
     }
 }
