51cc66098e80b370b1b218eb8a91f5df23c0cbf8
walterieux <david.desboeufs@hotmail.fr>
2020-03-24 01:34:20 +0100

    ViewPager2

---
+++ app/src/main/java/ch/epfl/polybazaar/UI/SliderItem.java
@@ -0,0 +1,16 @@
+package ch.epfl.polybazaar.UI;
+
+public class SliderItem {
+
+    //change here for a bitmap
+    private int image;
+
+    public SliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+}
