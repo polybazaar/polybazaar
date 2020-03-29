2cd2020395962d0361b5d1d35af78ebaff4033ae
Mathieu Caboche <mathieucaboche@gmail.com>
2020-03-29 11:07:30 +0200

    maps part operational

--- app/src/main/java/ch/epfl/polybazaar/map/MapsActivity.java
+++ app/src/main/java/ch/epfl/polybazaar/map/MapsActivity.java
@@ -47,8 +47,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -56,17 +55,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        checkLocationPermissions();
+        checkLocationPermissions(result -> mapInit());
     }
 
-    private void checkLocationPermissions() {
+    private void checkLocationPermissions(SuccessCallback successCallback) {
         SuccessCallback callback = result -> {
             if (result) {
                 mLocationPermissionGranted = true;
-                mapInit();
             } else {
                 mLocationPermissionGranted = false;
             }
+            successCallback.onCallback(result);
         };
         assertPermission(this, "ACCESS_FINE_LOCATION",
                 "Location is required to show your position on the map",
@@ -76,8 +75,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void mapInit() {
         if (mMap != null) {
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            }
             mMap.getUiSettings().setZoomControlsEnabled(false);
             // EPFL Marker
             //mMap.addMarker(new MarkerOptions().position(EPFL_LOCATION).title("EPFL"));
