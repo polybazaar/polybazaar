da4a8ab1c9bd293bfaeb9b2b2cd78435886e2c64
armen-h <armen.homberger@epfl.ch>
2020-04-02 17:16:42 +0200

    add tests for new utilities methods

---
+++ app/src/androidTest/java/ch/epfl/polybazaar/UtilitiesTest.java
@@ -0,0 +1,43 @@
+package ch.epfl.polybazaar;
+
+import android.content.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.epfl.polybazaar.login.AppUser;
+import ch.epfl.polybazaar.login.AuthenticatorFactory;
+import ch.epfl.polybazaar.login.MockAuthenticator;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+
+
+public class UtilitiesTest {
+
+    MockAuthenticator auth;
+    Context context;
+
+    @Before
+    public void init() {
+        auth = MockAuthenticator.getInstance();
+        AuthenticatorFactory.setDependency(auth);
+    }
+
+    @Test
+    public void testGetUserLoggedIn() {
+        auth.signIn(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
+        AppUser user = Utilities.getUser();
+        assertEquals(MockAuthenticator.TEST_USER_EMAIL, user.getEmail());
+        assertEquals(MockAuthenticator.TEST_USER_NICKNAME, user.getNickname());
+        assertTrue(Utilities.checkUserLoggedIn(context));
+    }
+
+    @Test
+    public void testUserNotLoggedIn() {
+        auth.signOut();
+        AppUser user = Utilities.getUser();
+        assertEquals(null, user);
+    }
+
+}
