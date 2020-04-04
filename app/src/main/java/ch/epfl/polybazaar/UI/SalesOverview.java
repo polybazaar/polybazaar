80ed562c6e00f00344c14eed3f46e453922861f3
Toussain <toussain@toussain.com>
2020-04-05 01:35:28 +0200

    selecting a category shows now all items in subcategories.

--- app/src/main/java/ch/epfl/polybazaar/UI/SalesOverview.java
+++ app/src/main/java/ch/epfl/polybazaar/UI/SalesOverview.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,9 +19,13 @@ import ch.epfl.polybazaar.R;
 import ch.epfl.polybazaar.SaleDetails;
 import ch.epfl.polybazaar.category.Category;
 import ch.epfl.polybazaar.category.CategoryRepository;
+import ch.epfl.polybazaar.category.RootCategory;
 import ch.epfl.polybazaar.category.StringCategory;
 import ch.epfl.polybazaar.database.callback.LiteListingCallback;
+import ch.epfl.polybazaar.database.callback.StringListCallback;
+import ch.epfl.polybazaar.listing.ListingDatabase;
 import ch.epfl.polybazaar.litelisting.LiteListing;
+import ch.epfl.polybazaar.litelisting.LiteListingDatabase;
 
 import static ch.epfl.polybazaar.litelisting.LiteListingDatabase.fetchLiteListing;
 
@@ -35,6 +40,8 @@ public class SalesOverview extends AppCompatActivity {
     private int positionInIDList = 0;
     private Spinner categorySelector;
     private final String DEFAULT_SPINNER_TEXT = "Select category...";
+    private String currentQuery = DEFAULT_SPINNER_TEXT;
+    private Category currentCategory = new RootCategory();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +51,11 @@ public class SalesOverview extends AppCompatActivity {
         IDList = new ArrayList<>();
         liteListingList = new ArrayList<>();
 
+        //setup the categories list
         categorySelector = findViewById(R.id.categorySelector);
-        setupSpinner(categorySelector, categoriesWithDefaultText(CategoryRepository.categories));
+        List<Category> categories = new ArrayList<>(CategoryRepository.categories);
+        categories.add(0,new RootCategory());
+        setupSpinner(categorySelector,categories);
 
         // Lookup the recyclerview in activity layout
         RecyclerView rvLiteListings = findViewById(R.id.rvLiteListings);
@@ -90,6 +100,104 @@ public class SalesOverview extends AppCompatActivity {
      * Create a graphical overview of LiteListings from database
      */
     public void loadLiteListingOverview() {
+        if(!currentCategory.equals(new RootCategory())){
+            loadLiteListingsfromCategory();
+        }else{
+            loadAllLiteListings();
+        }
+    }
+
+    /**
+     * Returns the list of lite listings shown currently
+     * @return list of lite listings
+     */
+    public List<LiteListing> getLiteListingList() {
+        return liteListingList;
+    }
+
+    private void setupSpinner(Spinner spinner, List<Category> categories){
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                //refresh currentQuery
+                Category selectedCategory = (Category)parent.getItemAtPosition(position);
+                //loadLiteListingQueryOverview(selectedCategory.toString());
+                currentQuery = selectedCategory.toString();
+                currentCategory = selectedCategory;
+                loadLiteListingOverview();
+
+                //re-init layout
+                liteListingList = new ArrayList<>();
+                IDList = new ArrayList<>();
+                positionInIDList = 0;
+                // Lookup the recyclerview in activity layout
+                RecyclerView rvLiteListings = findViewById(R.id.rvLiteListings);
+
+                // Create adapter passing in the sample LiteListing data
+                adapter = new LiteListingAdapter(liteListingList);
+
+                adapter.setOnItemClickListener(new LiteListingAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view) {
+                        int viewID = view.getId();
+                        String listingID = adapter.getListingID(viewID);
+                        Intent intent = new Intent(SalesOverview.this, SaleDetails.class);
+                        intent.putExtra("listingID", listingID);
+                        startActivity(intent);
+                    }
+                });
+
+                // Attach the adapter to the recyclerview to populate items
+                rvLiteListings.setAdapter(adapter);
+                // Set layout manager to position the items
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                rvLiteListings.setLayoutManager(linearLayoutManager);
+
+
+
+
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Do nothing
+            }
+        });
+    }
+
+    private void loadLiteListingsfromCategory(){
+        List<Category> allCategories = getAllSubCategories(currentCategory);
+        allCategories.add(currentCategory);
+
+        for(Category category : allCategories) {
+            LiteListingDatabase.queryLiteListingStringEquality("category", category.toString(), result -> {
+
+                for (String l : result) {
+                    IDList.add(l);      // create deep copy of ID list if list is empty
+                }
+
+                LiteListingCallback callbackLiteListing = new LiteListingCallback() {
+                    @Override
+                    public void onCallback(LiteListing result) {
+                        liteListingList.add(result);
+                        adapter.notifyItemInserted(liteListingList.size() - 1);
+                    }
+                };
+
+                int size = IDList.size();
+                for (int i = positionInIDList; i < (positionInIDList + EXTRALOAD) && i < size; i++) {
+                    fetchLiteListing(IDList.get(i), callbackLiteListing);
+                    positionInIDList++;
+                }
+            });
+        }
+    }
+    private void loadAllLiteListings(){
         LiteListing.retrieveAll().addOnSuccessListener(result -> {
             if(IDList.isEmpty()) {
                 for (LiteListing l : result) {
@@ -109,35 +217,20 @@ public class SalesOverview extends AppCompatActivity {
                 positionInIDList++;
             }
         });
+
     }
 
-    /**
-     * Returns the list of lite listings shown currently
-     * @return list of lite listings
-     */
-    public List<LiteListing> getLiteListingList() {
-        return liteListingList;
-    }
-
-    private void setupSpinner(Spinner spinner, List<Category> categories){
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categories);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Category selectedCategory = (Category)parent.getItemAtPosition(position);
+    List<Category> getAllSubCategories(Category category){
+        if(!category.hasSubCategories()){
+            return new ArrayList<>();
+        }else{
+            List<Category> subCategories = category.subCategories();
+            List<Category> allCategories = new ArrayList<>(subCategories);
+            for(Category cat : subCategories){
+                allCategories.addAll(getAllSubCategories(cat));
             }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //Do nothing
-            }
-        });
-    }
-    private List<Category> categoriesWithDefaultText(List<Category> categories){
-        List<Category> categoriesWithDefText = new ArrayList<>(categories);
-        categoriesWithDefText.add(0, new StringCategory(DEFAULT_SPINNER_TEXT));
-        return categoriesWithDefText;
+            return allCategories;
+        }
     }
 
 }
