dda856dd48862b31d92ecbd61742a7204180b089
Benoit <benoit.maillard@epfl.ch>
2020-04-17 15:08:54 +0200

    Remove obsolete code

--- app/src/main/java/ch/epfl/polybazaar/database/callback/ListingImageCallback.java
+++
@@ -1,8 +0,0 @@
-package ch.epfl.polybazaar.database.callback;
-
-import ch.epfl.polybazaar.listingImage.ListingImage;
-
-public interface ListingImageCallback {
-
-    void onCallback(ListingImage result);
-}
