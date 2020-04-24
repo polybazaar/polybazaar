986662a5205c010191843d1db61cc94c312d2828
Mathieu Caboche <mathieucaboche@gmail.com>
2020-04-24 16:51:55 +0200

    UI ok

--- app/src/main/java/ch/epfl/polybazaar/UserProfileActivity.java
+++ app/src/main/java/ch/epfl/polybazaar/UserProfileActivity.java
@@ -1,5 +1,12 @@
 package ch.epfl.polybazaar;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -120,5 +127,31 @@ public class UserProfileActivity extends AppCompatActivity {
                 displaySavedListings(this, ownListingsIds);
             }
         });
+    }
+
+    public void viewFavorites(View view) {
+
+    }
+
+    // taken from: https://stackoverflow.com/questions/12944275/crop-image-as-circle-in-android
+    private Bitmap getRoundedCroppedBitmap(Bitmap bitmap) {
+        int widthLight = bitmap.getWidth();
+        int heightLight = bitmap.getHeight();
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(output);
+        Paint paintColor = new Paint();
+        paintColor.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        RectF rectF = new RectF(new Rect(0, 0, widthLight, heightLight));
+
+        canvas.drawRoundRect(rectF, widthLight / 2 ,heightLight / 2,paintColor);
+
+        Paint paintImage = new Paint();
+        paintImage.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawBitmap(bitmap, 0, 0, paintImage);
+
+        return output;
     }
 }
