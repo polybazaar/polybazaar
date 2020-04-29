48636e79da7131677cc39e3d4d4820d2aa805c51
Mathieu Caboche <mathieucaboche@gmail.com>
2020-04-29 12:23:17 +0200

    UserProfile Covered

--- app/src/androidTest/java/ch/epfl/polybazaar/endToEnd/UserProfileTest.java
+++ app/src/androidTest/java/ch/epfl/polybazaar/endToEnd/UserProfileTest.java
@@ -1,21 +1,25 @@
 package ch.epfl.polybazaar.endToEnd;
 
 import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.view.View;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Tasks;
 
-import org.junit.FixMethodOrder;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import ch.epfl.polybazaar.MainActivity;
@@ -32,20 +36,28 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static androidx.test.runner.lifecycle.Stage.RESUMED;
 import static ch.epfl.polybazaar.database.datastore.DataStoreFactory.useMockDataStore;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertEquals;
 
 public class UserProfileTest {
 
+    private static final long SLEEP_TIME = 2000;
     private static Authenticator authenticator;
     boolean signedInFlag;
     @Rule
@@ -66,11 +78,49 @@ public class UserProfileTest {
     };
 
     @Test
+    public void ChangeProfilePictureTest() throws InterruptedException {
+        signInWithFromMainActivity();
+        onView(withId(R.id.profileButton)).perform(click());
+        addImage();
+        onView(withId(R.id.saveProfileButton)).perform(scrollTo(), click());
+        activityRule.getActivity().sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+    }
+
+    private void addImage() throws InterruptedException {
+        Resources resources = getInstrumentation().getTargetContext().getResources();
+        Uri imageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                resources.getResourcePackageName(R.mipmap.ic_launcher) + '/' +
+                resources.getResourceTypeName(R.mipmap.ic_launcher) + '/' +
+                resources.getResourceEntryName(R.mipmap.ic_launcher));
+        closeSoftKeyboard();
+        Intent galleryIntent = new Intent();
+        galleryIntent.setData(imageUri);
+        Instrumentation.ActivityResult galleryResult = new Instrumentation.ActivityResult(Activity.RESULT_OK, galleryIntent);
+        Matcher<Intent> expectedGalleryIntent = allOf(hasAction(Intent.ACTION_PICK), hasData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+        Intents.init();
+        intending(expectedGalleryIntent).respondWith(galleryResult);
+        getActivityInstance();
+        onView(withId(R.id.profilePicture)).perform(click());
+        Thread.sleep(SLEEP_TIME);
+        onView(withText(R.string.library))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
+        intended(expectedGalleryIntent);
+        Intents.release();
+        Thread.sleep(SLEEP_TIME);
+        onView(withText(R.string.alert_close))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
+    }
+
+    @Test
     public void testNameChangesWorks() throws InterruptedException {
         String newNickname = "new Nickname";
         String newFirstName = "Aurelien";
         String newLastName = "Queloz";
-        signInWithFromMainActivity(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
+        signInWithFromMainActivity();
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.nicknameSelector)).perform(scrollTo(), clearText(), typeText(newNickname));
         closeSoftKeyboard();
@@ -91,7 +141,7 @@ public class UserProfileTest {
     @Test
     public void testPasswordChangeWorks() throws InterruptedException {
         String newPassword = "mynewpassword";
-        signInWithFromMainActivity(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
+        signInWithFromMainActivity();
         onView(withId(R.id.profileButton)).perform(click());
 
         onView(withId(R.id.newPassword)).perform(scrollTo());
@@ -111,7 +161,7 @@ public class UserProfileTest {
 
     @Test
     public void userHasNoOwnListings() {
-        signInWithFromMainActivity(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
+        signInWithFromMainActivity();
         onView(withId(R.id.profileButton)).perform(click());
         closeSoftKeyboard();
         onView(withId(R.id.viewOwnListingsButton)).perform(click());
@@ -121,17 +171,17 @@ public class UserProfileTest {
                 .check(matches(isDisplayed()));
     }
 
-    private void signInWithFromMainActivity(String email, String password){
+    private void signInWithFromMainActivity(){
         onView(withId(R.id.authenticationButton)).perform(click());
-        onView(withId(R.id.emailInput)).perform(typeText(email));
+        onView(withId(R.id.emailInput)).perform(typeText(MockAuthenticator.TEST_USER_EMAIL));
         closeSoftKeyboard();
-        onView(withId(R.id.passwordInput)).perform(typeText(password));
+        onView(withId(R.id.passwordInput)).perform(typeText(MockAuthenticator.TEST_USER_PASSWORD));
         closeSoftKeyboard();
         onView(withId(R.id.loginButton)).perform(click());
         onView(withId(R.id.toMainButton)).perform(click());
 }
 
-    Activity currentActivity = null;
+    private Activity currentActivity = null;
 
     public Activity getActivityInstance(){
         getInstrumentation().runOnMainSync(new Runnable() {
