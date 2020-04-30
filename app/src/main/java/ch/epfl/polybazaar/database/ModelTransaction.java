6f342b96492f8c770f2d3b1c8723f24b431b3dd6
Jan Maxime <maxime.jan@epfl.ch>
2020-05-01 01:53:01 +0200

    Correct locally the merge done online

--- app/src/main/java/ch/epfl/polybazaar/database/ModelTransaction.java
+++ app/src/main/java/ch/epfl/polybazaar/database/ModelTransaction.java
@@ -67,12 +67,6 @@ public final class ModelTransaction {
                 .onSuccessTask(querySnapshot -> Tasks.forResult(toModels(querySnapshot, clazz)));
     }
 
-    public static <T> Task<Void> updateField(String collectionPath, String id, String field, T updatedValue) {
-        DataStore db = DataStoreFactory.getDependency();
-        return db.updateField(collectionPath, id, field, updatedValue)
-                .onSuccessTask(querySnapshot -> Tasks.forResult(null));
-    }
-
     /**
      * Updates the given field of the document with the updated value
      * @param collectionPath name of the collection
