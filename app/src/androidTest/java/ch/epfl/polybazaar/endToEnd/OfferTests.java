5e39f7eaa5d834ab08f202de73ba16175a6beff8
Mathieu Caboche <mathieucaboche@gmail.com>
2020-05-05 17:49:34 +0200

    tests pass

--- app/src/androidTest/java/ch/epfl/polybazaar/endToEnd/OfferTests.java
+++ app/src/androidTest/java/ch/epfl/polybazaar/endToEnd/OfferTests.java
@@ -2,13 +2,17 @@ package ch.epfl.polybazaar.endToEnd;
 
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.firebase.Timestamp;
+
 import org.junit.Rule;
 import org.junit.Test;
 
+
 import ch.epfl.polybazaar.MainActivity;
 import ch.epfl.polybazaar.R;
-import ch.epfl.polybazaar.UI.SliderAdapter;
-import ch.epfl.polybazaar.login.Authenticator;
+import ch.epfl.polybazaar.chat.ChatMessage;
+import ch.epfl.polybazaar.listing.Listing;
+import ch.epfl.polybazaar.litelisting.LiteListing;
 import ch.epfl.polybazaar.login.AuthenticatorFactory;
 import ch.epfl.polybazaar.login.MockAuthenticator;
 import ch.epfl.polybazaar.user.User;
@@ -19,14 +23,11 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.polybazaar.database.datastore.DataStoreFactory.useMockDataStore;
-import static ch.epfl.polybazaar.testingUtilities.SignInUtilities.createAccountAndBackToLoginFromLoginActivity;
-import static ch.epfl.polybazaar.testingUtilities.SignInUtilities.signInWithFromMainActivity;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.is;
 
 public class OfferTests {
 
@@ -34,8 +35,10 @@ public class OfferTests {
 
     private final String Listing1Name = "LOL";
     private final String Listing1Price = "123";
-    private final String Listing2Name = "FUN";
-    private final String Listing2Price = "321";
+    private final String TestUser2Email = "test.magicuser@epfl.ch";
+    private final String TestUser2NickName = "lolMan";
+    private final String TestUser2Password = "password123";
+    private String testListing1ID;
 
     @Rule
     public final ActivityTestRule<MainActivity> activityRule =
@@ -44,8 +47,23 @@ public class OfferTests {
                 protected void beforeActivityLaunched() {
                     useMockDataStore();
                     AuthenticatorFactory.setDependency(MockAuthenticator.getInstance());
-                    User testUser = new User("nickname", MockAuthenticator.TEST_USER_EMAIL);
-                    testUser.save();
+                    User testUser1 = new User("nickname", MockAuthenticator.TEST_USER_EMAIL);
+                    testUser1.save();
+                    MockAuthenticator.getInstance().createUser(TestUser2Email, TestUser2NickName, TestUser2Password);
+                    User testUser2 = new User(TestUser2NickName, TestUser2Email);
+                    testUser2.save();
+                    Listing testListing1 = new Listing(Listing1Name, "", Listing1Price, MockAuthenticator.TEST_USER_EMAIL, "none");
+                    testListing1.saveWithLiteVersion();
+                    try {
+                        Thread.sleep(SLEEP_TIME);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ChatMessage message = new ChatMessage(TestUser2Email, MockAuthenticator.TEST_USER_EMAIL,
+                            testListing1.getId(), ChatMessage.OFFER_MADE+ "15.0", new Timestamp(12, 13));
+                    message.save();
+                    testListing1ID = testListing1.getId();
+                    MockAuthenticator.getInstance().signIn(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
                 }
                 @Override
                 protected void afterActivityFinished() {
@@ -54,67 +72,39 @@ public class OfferTests {
             };
 
     @Test
-    public void refuseThenAcceptOfferTest() throws InterruptedException {
-        // setup
-        onView(withId(R.id.action_profile)).perform(click());
-        createAccountAndBackToLoginFromLoginActivity("test.magicuser@epfl.ch", "lolMan", "password123");
-        setupListingAndConversation(Listing1Name, Listing1Price);
-        // refuse offer
+    public void doOfferTest() {
+        MockAuthenticator.getInstance().signIn(TestUser2Email, TestUser2Password);
+        onView(withId(R.id.action_home)).perform(click());
+        onView(withText(Listing1Name)).perform(click());
         doAllOfferOptions();
-        gotToConversation(Listing1Name);
-        onView(withId(R.id.refuse_offer_button)).perform(click());
+        ChatMessage.fetchMessagesFrom(TestUser2Email).addOnSuccessListener(chatMessages -> {
+            assertThat(chatMessages.get(0).getMessage(), is(ChatMessage.OFFER_MADE+ "15.0"));
+        });
         pressBack();
-        onView(withId(R.id.action_home)).perform(click());
-        onView(withText("CHF " + Listing1Price)).perform(click());
-        onView(withId(R.id.price)).check(matches(withText("CHF " + Listing1Price)));
-        pressBack();
-        // accept offer
-        signOutFromMainActivity();
-        setupListingAndConversation(Listing2Name, Listing2Price);
-        onView(withId(R.id.makeOffer)).perform(click());
-        onView(withId(R.id.offer)).perform(typeText("15"));
-        onView(withId(R.id.makeOfferNow)).perform(click());
-        pressBack();
-        gotToConversation(Listing2Name);
-        onView(withId(R.id.accept_offer_button)).perform(click());
-        pressBack();
-        onView(withId(R.id.action_home)).perform(click());
-        onView(withText(activityRule.getActivity().getString(R.string.sold))).perform(click());
-        onView(withId(R.id.price)).check(matches(withText(activityRule.getActivity().getString(R.string.sold))));
-        onView(withId(R.id.makeOffer)).check(matches(not(isDisplayed())));
-        onView(withId(R.id.buyNow)).check(matches(not(isDisplayed())));
     }
 
-    private void addItemFromFillListing(String name, String price) {
-        onView(withId(R.id.titleSelector)).perform(scrollTo(), typeText(name));
-        closeSoftKeyboard();
-        onView(withId(R.id.priceSelector)).perform(scrollTo(), typeText(price));
-        closeSoftKeyboard();
-        onView(withId(R.id.submitListing)).perform(scrollTo(), click());
-    }
-
-    private void signOutFromMainActivity() {
-        onView(withId(R.id.action_profile)).perform(click());
-        onView(withId(R.id.signOutButton)).perform(scrollTo(), click());
-    }
-
-
-    private void setupListingAndConversation(String name, String price){
-        signInWithFromMainActivity(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
-        onView(withId(R.id.action_add_item)).perform(click());
-        closeSoftKeyboard();
-        addItemFromFillListing(name, price);
-        signOutFromMainActivity();
-        signInWithFromMainActivity("test.magicuser@epfl.ch", "password123");
-        onView(withText(name)).perform(click());
-    }
-
-    private void gotToConversation(String name) throws InterruptedException {
-        signOutFromMainActivity();
-        signInWithFromMainActivity(MockAuthenticator.TEST_USER_EMAIL, MockAuthenticator.TEST_USER_PASSWORD);
+    @Test
+    public void acceptOfferTest() {
         onView(withId(R.id.action_messages)).perform(click());
-        onView(withText(name)).perform(click());
-        Thread.sleep(SLEEP_TIME);
+        onView(withText(Listing1Name)).perform(click());
+        onView(withId(R.id.accept_offer_button)).perform(click());
+        Listing.fetch(testListing1ID).addOnSuccessListener(listing -> {
+            assertThat(listing.getListingActive(), is(false));
+            assertThat(listing.getPrice(), is(activityRule.getActivity().getString(R.string.sold)));
+        });
+        LiteListing.fetch(testListing1ID).addOnSuccessListener(listing -> {
+            assertThat(listing.getPrice(), is(activityRule.getActivity().getString(R.string.sold)));
+        });
+    }
+
+    @Test
+    public void refuseOfferTest() {
+        onView(withId(R.id.action_messages)).perform(click());
+        onView(withText(Listing1Name)).perform(click());
+        onView(withId(R.id.refuse_offer_button)).perform(click());
+        Listing.fetch(testListing1ID).addOnSuccessListener(listing -> {
+            assertThat(listing.getListingActive(), is(true));
+        });
     }
 
     private void doAllOfferOptions() {
@@ -126,7 +116,7 @@ public class OfferTests {
         onView(withId(R.id.cancelOfferMaking)).perform(click());
         onView(withId(R.id.makeOffer)).perform(scrollTo(), click());
         onView(withId(R.id.offer)).perform(typeText("15"));
-        onView(withId(R.id.makeOfferNow)).perform(scrollTo(), click());
+        onView(withId(R.id.makeOfferNow)).perform(click());
         pressBack();
     }
 }
