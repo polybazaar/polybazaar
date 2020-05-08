002ef9e19a8269c89a8910ebb254852c80134841
armen-h <armen.homberger@epfl.ch>
2020-05-08 08:48:10 +0200

    add clickable favorites items in salesOverview for tests

--- app/src/androidTest/java/ch/epfl/polybazaar/FavoritesTest.java
+++ app/src/androidTest/java/ch/epfl/polybazaar/FavoritesTest.java
@@ -6,6 +6,7 @@ import androidx.test.rule.ActivityTestRule;
 import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import ch.epfl.polybazaar.UI.SalesOverview;
 import ch.epfl.polybazaar.login.AuthenticatorFactory;
@@ -13,7 +14,14 @@ import ch.epfl.polybazaar.login.MockAuthenticator;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.polybazaar.database.datastore.DataStoreFactory.useMockDataStore;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
 
 
 public class FavoritesTest {
@@ -35,23 +43,23 @@ public class FavoritesTest {
     }
 
 
-    /*@Test
+    @Test
     public void favoritesUserNotLoggedIn() {
         clickButton(withId(R.id.favoritesOverview));
 
         onView(withText(R.string.sign_in_required))
                 .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
                 .check(matches(isDisplayed()));
-    }*/
+    }
 
-   /* @Test public void favoritesListIsEmpty() {
+   @Test public void favoritesListIsEmpty() {
         auth.signIn("test.user@epfl.ch", "abcdef");
         clickButton(withId(R.id.favoritesOverview));
 
         onView(withText(R.string.no_favorites))
                 .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
                 .check(matches(isDisplayed()));
-    }*/
+    }
     
     private void clickButton(Matcher<View> object) {
         onView(object).perform(click());
