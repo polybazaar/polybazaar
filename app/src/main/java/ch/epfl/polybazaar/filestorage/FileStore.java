893ccb793461714344e40b36936c56f1ec352d52
Benoit <benoit.maillard@epfl.ch>
2020-05-12 22:36:17 +0200

    Add FileStore interface

---
+++ app/src/main/java/ch/epfl/polybazaar/filestorage/FileStore.java
@@ -0,0 +1,25 @@
+package ch.epfl.polybazaar.filestorage;
+
+import com.google.android.gms.tasks.Task;
+
+import java.io.InputStream;
+
+/**
+ * Offers methods to communicate with cloud storage
+ */
+public interface FileStore {
+    /**
+     * Fetch a resource from the cloud
+     * @param id resource name
+     * @return task containing a stream with the data, null if the resource does not exist
+     */
+    Task<InputStream> fetch(String id);
+
+    /**
+     * Store a resource on the cloud
+     * @param id resource name
+     * @param data stream from which the data can be read
+     * @return void task
+     */
+    Task<Void> store(String id, InputStream data);
+}
